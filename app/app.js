ba5befcfcbb6419bf95a04519e12edfce8a3c2cb
Knut Behrends <knb@tnp-online.de>
2018-10-07 16:25:29 +0200

    add dist to .gitignore

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
-(function () {
-  "use strict";
 
-  angular.module("solitaire", ["klondike", "ngDraggable"]);
-})();
+import "./klondike/scoring.js" // implies "use strict";
+angular.module("solitaire", ["klondike", "ngDraggable"]);
