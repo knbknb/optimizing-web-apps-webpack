b1b7941b916454eb780c5b14c6c8197e5f6d984a
Knut Behrends <knb@tnp-online.de>
2018-10-07 17:10:30 +0200

    add parseroptions {} to .eslintrc

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
-
 import "./klondike/scoring.js" // implies "use strict";
+import "./klondike/klondike.js";
+import "./klondike/board.js";
+import "./klondike/game.js";
+
 angular.module("solitaire", ["klondike", "ngDraggable"]);
